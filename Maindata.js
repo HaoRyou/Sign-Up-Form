7ab99bf88ae3aeaab6fda891e1864e587915c8df
HaoRyou <158393369+HaoRyou@users.noreply.github.com>
2025-07-12 19:39:48 -0400

    modified: Try to finish the password check. Password check by using .value and change brdercolor if match.

--- Maindata.js
+++ Maindata.js
@@ -2,19 +2,18 @@ const password = document.getElementById("password");
 const confirm_password =document.getElementById("confirm-password");
 
 
-checkpassword(password,confirm_password);
+confirm_password.addEventListener('input', checkpassword(password,confirm_password));
 
 function checkpassword(a,b){
-    if (a==b){
-        password.classList.remove('invalid');
-        password.classList.add('valid');
-        confirm_password.classList.remove('invalid');
-        confirm_password.classList.add('valid');
+
+    console.log(a.value);
+    console.log(b.value);
+    if (a.value==b.value){
+        password.style.borderColor = "lightblue";
+        confirm_password.style.borderColor = "lightblue";
     }
     else{
-        password.classList.add('invalid');
-        password.classList.remove('valid');
-        confirm_password.classList.add('invalid');
-        confirm_password.classList.remove('valid');;
+        password.style.borderColor = "red";
+        confirm_password.style.borderColor = "red";
     }
 }
